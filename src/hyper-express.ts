907453b756750ff285b3874aeee02eb3e83204ac
faisolavolut <faisol@avolut.com>
2023-07-26 15:00:49 +0700

    Initial commit

---
+++ src/hyper-express.ts
@@ -0,0 +1,41 @@
+import HyperExpress, { MiddlewareHandler } from "hyper-express";
+const mime = require("mime-types")
+const { v4: uuidv4 } = require('uuid');
+const { pipeline } = require("node:stream/promises");
+const fs = require("node:fs");
+import path from "path";
+
+const webserver = new HyperExpress.Server({
+  max_body_length: Number.MAX_SAFE_INTEGER,
+});
+
+// Create GET route to serve 'Hello World'
+webserver.get("/", (req, res) => {
+  res.send("ok");
+});
+
+webserver.any("/upload", async (req, res) => {
+  await req.multipart(async (field) => {
+    if (field.file) {
+      let pathFile = path.join(
+        "./storage",
+        "images",
+        uuidv4() + path.extname(field.file.name || ''),
+        
+      );
+      console.log(pathFile)
+      try {
+        await pipeline(field.file.stream, fs.createWriteStream(pathFile));
+      } catch (err) {
+        console.log("error", err);
+      }
+    }
+  });
+  res.send("ok");
+});
+
+// Activate webserver by calling .listen(port, callback);
+webserver
+  .listen(12300)
+  .then(() => console.log(`Webserver started on port ${'http://localhost:12300'}`))
+  .catch(() => console.log("Failed to start webserver on port 80"));
